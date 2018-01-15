5ccb9f6de07e009567dc097d5c11c1fe3e4da729
QingqingLin <16120231@bjtu.edu.cn>
2018-01-15 20:44:29 +0800

    Signed-off-by: QingqingLin <16120231@bjtu.edu.cn>

--- 服务器/SuperServer/MainForm.Designer.cs
+++ 服务器/SuperServer/MainForm.Designer.cs
@@ -77,5 +77,83 @@
         private System.Windows.Forms.ContextMenuStrip myMenu;
         private System.Windows.Forms.ToolStripMenuItem 退出;
     }
+
+    partial class CopyOfMainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CopyOfMainForm));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.myMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.退出 = new System.Windows.Forms.ToolStripMenuItem();
+            this.myMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.myMenu;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "MonitorServer";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            // 
+            // myMenu
+            // 
+            this.myMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.退出});
+            this.myMenu.Name = "myMenu";
+            this.myMenu.Size = new System.Drawing.Size(101, 26);
+            // 
+            // 退出
+            // 
+            this.退出.Name = "退出";
+            this.退出.Size = new System.Drawing.Size(100, 22);
+            this.退出.Text = "退出";
+            this.退出.Click += new System.EventHandler(this.toolMenuCancel_Click);
+            // 
+            // CopyOfMainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Name = "CopyOfMainForm";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.myMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip myMenu;
+        private System.Windows.Forms.ToolStripMenuItem 退出;
+    }
 }
 
